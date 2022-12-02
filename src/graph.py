c44c730198cf2f7c1b51a444fd6fe145f4f50030
ymj5185 <ymj5185@psu.edu>
2022-12-02 16:37:07 -0500

    add comments

--- src/graph.py
+++ src/graph.py
@@ -1,11 +1,13 @@
 MAX_NUM_NODES = 100
 
+
 class EdgeException(Exception):
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return repr(self.value)
+
 
 class Edge:
     def __init__(self, u, v):
@@ -26,12 +28,13 @@ class Edge:
     def check(self):
         for i in range(2):
             if self.ends[i] < 0 or self.ends[i] >= MAX_NUM_NODES:
-                raise EdgeException(('Node {0} out of range [0--{1}] '+
-                    'in edge {2}.').format(self.ends[i], MAX_NUM_NODES-1,
-                        self))
+                raise EdgeException(('Node {0} out of range [0--{1}] ' +
+                                     'in edge {2}.').format(self.ends[i], MAX_NUM_NODES - 1,
+                                                            self))
         if self.ends[0] == self.ends[1]:
-            raise EdgeException('Self-loop not allowed in '+
-                    'edge {0}.'.format(self))
+            raise EdgeException('Self-loop not allowed in ' +
+                                'edge {0}.'.format(self))
+
 
 def make_graph(edge_set, num_vertex=MAX_NUM_NODES):
     G = Graph(num_vertex)
@@ -41,15 +44,17 @@ def make_graph(edge_set, num_vertex=MAX_NUM_NODES):
 
     return G
 
+
 class Graph:
     def __init__(self, num_nodes):
-        self.neighbors = [ [] for i in range(num_nodes) ]
+        self.neighbors = [[] for i in range(num_nodes)]
         self.num_of_components = 0
         self.num_nodes = 0
         self.num_leaves = 0
         self.num_vertex = num_nodes
         self.has_cycle = False
 
+    # Add an edge to graph
     def add_edge(self, e):
         self.add_edge_uv(e.ends[0], e.ends[1])
 
@@ -60,19 +65,23 @@ class Graph:
     def add_directed_edge_uv(self, u, v):
         self.neighbors[u].append(v)
 
+    # Return if the edges in one component
     def edges_in_one_component(self):
         return self.num_of_components == 1
-    
+
+    # Return the number of vertex of the graph
     def get_num_vertex(self):
         return self.num_vertex
 
+    # Get the # of nodes, # of leaves, # of components, and check if the graph contain a cycle
     def search(self):
-        visited = [ False for i in range(MAX_NUM_NODES) ]
+        visited = [False for i in range(MAX_NUM_NODES)]
         self.num_nodes = 0
         self.num_leaves = 0
         self.num_of_components = 0
         self.has_cycle = False
 
+        # Run DFS to check if the graph contains a cycle
         def dfs(node, parent):
             visited[node] = True
             for u in self.neighbors[node]:
